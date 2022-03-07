4df8fcb3c0ca7c11b9ca96921b2e5bdb92799bcd
igor98santos <igmascaras58@gmail.com>
2022-03-07 16:01:14 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,25 @@
+import {ReactComponent as GithubIcon} from '../../assets/img/github.svg';
+import "./style.css"
+function Navbar() {
+    return (
+      <header>
+        <nav className="container">
+          <div className="dsmovie-nav-content">
+            <h1>DSMovie</h1>
+            <a
+              href="https://github.com/igor98santos/dsmovie"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="dsmovie-contact-container">
+                <GithubIcon />
+                <p className="dsmovie-contact-link">/GitHub da aplicação</p>
+              </div>
+            </a>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+  
+  export default  Navbar;
