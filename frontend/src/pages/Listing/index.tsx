e2b73bf2ca40d5372f7bb50a2d087dea4afe0fa1
igor98santos <igmascaras58@gmail.com>
2022-03-11 12:02:59 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -6,39 +6,49 @@ import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/pagination";
 
 function Listing() {
-  const [pageNumber, setPageNumber] = useState()
 
-  useEffect(() =>{
-    axios.get(`${BASE_URL}/movies?size=12&page=1`).then(response =>{
-      const data = response.data as MoviePage;
-      setPageNumber(data.number);
-    })
-  }, [])
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const [page, setPage] = useState<MoviePage>({
+      content: [],
+      last: true,
+      totalPages: 0,
+      totalElements: 0,
+      size: 12,
+      number: 0,
+      first: true,
+      numberOfElements: 0,
+      empty: true
+  });
+
+  useEffect(() => {
+      axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+          .then(response => {
+              const data = response.data as MoviePage;
+              setPage(data);
+          });
+  }, [pageNumber]);
+
+  const handlePageChange = (newPageNumber : number) => {
+      setPageNumber(newPageNumber);
+  }
 
   return (
-    <>
-    <p>{pageNumber}</p>
-      <Pagination />
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
+      <>
+          <Pagination page={page} onChange={handlePageChange} />
+
+          <div className="container">
+              <div className="row">
+                  {page.content.map(movie => (
+                      <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                          <MovieCard movie={movie} />
+                      </div>
+                  )
+                  )}
+              </div>
           </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-        </div>
-      </div>
-    </>
+      </>
   );
 }
+
 export default Listing;
