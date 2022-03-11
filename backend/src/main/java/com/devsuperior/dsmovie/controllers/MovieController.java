1c0146e6f86308ceecc173384fa8371a3ccd7f7e
igor98santos <igmascaras58@gmail.com>
2022-03-11 04:02:44 -0300

    Save Score

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
@@ -1,4 +1,5 @@
 package com.devsuperior.dsmovie.controllers;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,15 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmovie.dto.MovieDTO;
-import com.devsuperior.dsmovie.services.MovieServices;
-
+import com.devsuperior.dsmovie.services.MovieService;
 
 @RestController
 @RequestMapping(value = "/movies")
 public class MovieController {
 
 	@Autowired
-	private MovieServices service;
+	private MovieService service;
 	
 	@GetMapping
 	public Page<MovieDTO> findAll(Pageable pageable) {
